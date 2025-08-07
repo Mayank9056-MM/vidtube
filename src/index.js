711d5e38978975949dc547697fb2eb5a9f41df78
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-08-07 16:22:45 +0530

    Add Winston logger implementation for structured logging

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("hello from vidtube");
+import express from "express";
+import logger from "./logger";
+import morgan from "morgan";
+
+const app = express();
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
