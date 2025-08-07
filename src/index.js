83db9607e64bbb3df9f1760e83dbbf658322476d
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-08-07 20:54:48 +0530

    common middleware setup and db connection

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import express from "express";
-import logger from "./logger";
-import morgan from "morgan";
+import "dotenv/config";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-const morganFormat = ":method :url :status :response-time ms";
-
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
   })
-);
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
