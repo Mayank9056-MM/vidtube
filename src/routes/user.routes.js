00e4cef10977596d4c3da06d62da3903027aaffb
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-08-20 14:01:21 +0530

    write all routes for user controllers and fix logout api

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,23 @@
 import express from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controllers.js";
 import { verifyAuth } from "../middlewares/verifyAuth.js";
 
 const userRouter = express.Router();
+
+// unsecured routes
 
 userRouter.route("/register").post(
   upload.fields([
@@ -24,8 +34,20 @@ userRouter.route("/register").post(
 );
 
 userRouter.route("/login").post(loginUser);
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
 // secured routes
 userRouter.route("/logout").post(verifyAuth, logoutUser);
+userRouter.route("/change-password").post(verifyAuth, changeCurrentPassword);
+userRouter.route("/update-account").patch(verifyAuth, updateAccountDetails);
+userRouter.route("/current-user").get(verifyAuth, getCurrentUser);
+userRouter
+  .route("/avatar")
+  .patch(verifyAuth, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("/cover-image")
+  .patch(verifyAuth, upload.single("coverImage"), updateUserCoverImage);
+userRouter.route("/c/:username").get(verifyAuth, getUserChannelProfile);
+userRouter.route("/history").get(verifyAuth, getWatchHistory);
 
 export default userRouter;
